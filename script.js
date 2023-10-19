bb5ca57a8981860183fde3705c2b5729a88e371c
ReaganInsideJob <126184176+ReaganInsideJob@users.noreply.github.com>
2023-10-19 00:29:08 -0400

    Added project files

---
+++ script.js
@@ -0,0 +1,17 @@
+function playSound(audioName){
+  let audio = new Audio(audioName);
+  audio.loop=true;
+  audio.play();
+}
+playSound("A Whisper.mp3");
+
+
+function showMoreWords() {
+  var scrollContent = document.getElementById("scroll-content");
+  var scrollHeight = scrollContent.scrollHeight;
+  var scrollTop = scrollContent.scrollTop;
+  var clientHeight = scrollContent.clientHeight;
+  if (scrollHeight - (scrollTop + clientHeight) < 50) {
+    scrollContent.style.height = (scrollHeight + 500) + "px";
+  }
+}
